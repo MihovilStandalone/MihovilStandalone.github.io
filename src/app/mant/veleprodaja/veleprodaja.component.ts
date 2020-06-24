5b0f653a22c04c8cf91af1096474ada2a9b550a4
Mihovil Maricic <mihovil.maricic@tacta.io>
2020-06-24 20:03:19 +0200

    matejev dizajn

--- src/app/mant/veleprodaja/veleprodaja.component.ts
+++ src/app/mant/veleprodaja/veleprodaja.component.ts
@@ -6,26 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./veleprodaja.component.css']
 })
 export class VeleprodajaComponent implements OnInit {
-  janjetina: boolean=false;
-  junetina: boolean=false;
-  svinjetina: boolean=false;
-  patkaguska: boolean=false;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  changeJanjetina() {
-    this.janjetina = !this.janjetina;
-  }
-  changeJunetina() {
-    this.junetina = !this.junetina;
-  }
-  changeSvinjetina() {
-    this.svinjetina = !this.svinjetina;
-  }
-  changePatkaGuska() {
-    this.patkaguska = !this.patkaguska;
   }
 }
